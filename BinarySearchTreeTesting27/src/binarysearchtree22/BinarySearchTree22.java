973f807859c22c8d63125c534b7319ceee7cbe63
seank2014 <shaun.king21@gmail.com>
2019-07-03 13:50:09 -0400

    Day 27

---
+++ BinarySearchTreeTesting27/src/binarysearchtree22/BinarySearchTree22.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binarysearchtree22;
+
+/**
+ *
+ * @author seanking
+ */
+public class BinarySearchTree22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+//        EmptyBST e = new EmptyBST();
+//        NonEmptyBST n = new NonEmptyBST(5);
+//        
+//        
+//        Testers.checkIsEmpty(e);
+//        Testers.checkIsEmpty(n);
+//
+//        Testers.checkAddMemberCardinality(e, 5);
+//        Testers.checkAddMemberCardinality(n, 5);
+//        Testers.checkAddMemberCardinality(n, 6);
+        
+        int tests = 1000;
+        
+        for(int i = 0; i < tests; i++){
+            Tree t;
+            if(i % 10 == 0){
+                t = Testers.rndTree(0);
+            }else{
+                t = Testers.rndTree(10);          
+            }
+            Testers.checkAddMemberCardinality(t, i);
+            
+            
+        }
+
+    }
+
+}
---
+++ BinarySearchTreeTesting27/src/binarysearchtree22/Tree.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binarysearchtree22;
+
+/**
+ *
+ * @author seanking
+ */
+public interface Tree<D extends Comparable> {
+    
+    public boolean isEmpty();
+    public int cardinality();
+    public boolean member(D elt);
+    public NonEmptyBST<D> add(D elt);
+    
+    
+}
